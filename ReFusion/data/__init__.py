2a2800ad20c23bbbbbd0d45074258d79abca5915
Xiaoman Liu <135020518+Xiaoman916@users.noreply.github.com>
2026-04-05 10:47:17 +0800

    Delete ReFusion/data directory

--- ReFusion/data/__init__.py
+++
@@ -1,8 +0,0 @@
-from .data_loader import create_dataloaders, MultimodalFaultDataset, CANONICAL_FAULT_TYPES, MODALITY_GROUPS
-
-__all__ = [
-    "create_dataloaders",
-    "MultimodalFaultDataset",
-    "CANONICAL_FAULT_TYPES",
-    "MODALITY_GROUPS",
-]
